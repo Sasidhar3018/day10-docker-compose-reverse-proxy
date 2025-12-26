9886a638c4f3de0b5651fde6ecdd4a3a4ec7514f
Venkata Sasidhar Reddy <Sasidhar3018@gmail.com>
2025-12-26 09:34:50 +0000

    Add hostname-based response to verify container-level load balancing

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from datetime import datetime
 
-PORT = 8000
-
-class Handler(SimpleHTTPRequestHandler):
+class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Hello from Python Backend (via Nginx)")
+        hostname = socket.gethostname()
+        now = datetime.utcnow().isoformat()
 
-server = HTTPServer(("0.0.0.0", PORT), Handler)
-print(f"Backend running on port {PORT}")
+        response = f"""
+Hello from Python App
+---------------------
+Hostname: {hostname}
+Time (UTC): {now}
+"""
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(response.encode())
+
+server = HTTPServer(("0.0.0.0", 8000), Handler)
+print("Server started on port 8000")
 server.serve_forever()
